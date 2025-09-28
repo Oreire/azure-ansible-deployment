9da59f8bdd541e3ea41f00c910322a60aa31fc11
LAREDO LANCER <lancerajayi@yahoo.com>
2025-09-28 02:00:16 +0100

    Chnaged Azure SKU type to Standard

--- terra/main.tf
+++ terra/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_public_ip" "main" {
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Dynamic"
-  sku                 = "Basic"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "main" {
