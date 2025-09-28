b017591ba5ea140443dd252964a3971ed7b54ddf
LAREDO LANCER <lancerajayi@yahoo.com>
2025-09-28 02:39:26 +0100

    Refcatored VM Image

--- terra/backend.tf
+++ terra/backend.tf
@@ -7,3 +7,13 @@
 #         key                  = "terraform.tfstate"
 #     }
 # }
+
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "rg-terraform-state"
+#     storage_account_name = "tfstateaccount"
+#     container_name       = "tfstate"
+#     key                  = "terraform.tfstate"
+#   }
+# }
+
